a42f6727214b533e5742e1eed335da1028c43004
Ifrad Arnob <arnobifrad2@gmail.com>
2021-01-15 19:36:41 +0600

    first commit

---
+++ sumOfdigits.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+// #include <vector>
+using namespace std;
+
+int main()
+{
+    int t,n, total=0;
+    int array[100];
+    cin >> t >>n;
+
+    // vector<int> num; // all numbers
+
+
+    for (int i = 1; i < t; i++)
+    {
+        cin >> n;
+        total += array[t];
+        for(int i= 0;i< t; i++);
+        {
+            
+            cout << total;
+
+        }
+
+    }
+
+}
+
